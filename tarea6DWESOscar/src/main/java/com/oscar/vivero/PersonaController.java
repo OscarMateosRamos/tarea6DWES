22fb427ccc88a615681603f6a3132c4850a5eb9b
Osmara <dsi85944@educastur.es>
2025-02-09 12:52:15 +0100

    Cambios realizados en el controlador de las personas

--- tarea6DWESOscar/src/main/java/com/oscar/vivero/PersonaController.java
+++ tarea6DWESOscar/src/main/java/com/oscar/vivero/PersonaController.java
@@ -1,13 +1,11 @@
 package com.oscar.vivero;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.oscar.vivero.modelo.Credenciales;
 import com.oscar.vivero.modelo.Persona;
@@ -27,16 +25,47 @@ public class PersonaController {
 		model.addAttribute("personas", servPersona.vertodasPersonas());
 		return "listadodePersonas";
 	}
-	
-	
-	@PostMapping("/Crearpersonas")
-	public String InsertarPersonas(Model model) {
-		model.addAttribute("personas", servPersona.vertodasPersonas());
-		return "CrearPersona";
+
+	@PostMapping("/CamposPersona")
+	public String InsertarPlanta(@ModelAttribute Persona CrearPesonas, Model model) {
+
+		Persona p = new Persona();
+
+		String nombre = CrearPesonas.getNombre();
+		String email = CrearPesonas.getEmail();
+		String usuario = CrearPesonas.getCredencial().getUsuario();
+		String password = CrearPesonas.getCredencial().getPassword();
+
+		if (servCredenciales.existeCredencial(usuario)) {
+			model.addAttribute("error", "Ya existe el usuario: " + usuario);
+			return "CrearPersonas";
+		} else {
+			boolean personaValida = servPersona.validarPersona(nombre, email);
+
+			if (!personaValida) {
+				model.addAttribute("error", "Datos no validos para la persona ");
+				return "CrearPersonas";
+			} else {
+				Credenciales c = new Credenciales();
+
+				c.setUsuario(usuario);
+				c.setPassword(password);
+
+				servCredenciales.insertarCredencial(c);
+
+				p.setNombre(nombre);
+				p.setEmail(email);
+				p.setCredencial(c);
+
+				servPersona.insertarPersona(p);
+
+			}
+
+		}
+
+		return "/CrearPersonas";
 	}
-	
-	
-	
+
 	@GetMapping("/mostrarCrearPersonas")
 	public String mostrarFormulario(Model model) {
 		model.addAttribute("persona", new Persona());
